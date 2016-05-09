ac42bb6effa2db58b72db9937850f857e709c879
Chifeng Wen <daviesx66@gmail.com>
2016-05-09 00:46:47 -0700

    optimize the html page

--- medicom/api/bptable.js
+++ medicom/api/bptable.js
@@ -1,1 +1,10 @@
 import {Meteor} from 'meteor/meteor';
+
+
+export function BPTble() {
+        this.__dates = [];
+        this.__values = [];
+        
+        this.construct_from_csv_stream = function(stream) {
+        }
+}
