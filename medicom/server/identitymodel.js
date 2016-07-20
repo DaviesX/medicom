e882c32efdea6ffed5000237268aee3c5f3df2db
Chifeng Wen <daviesx66@gmail.com>
2016-07-19 20:33:15 -0700

    fixed a bug in the verification process & added more detection code.

--- medicom/server/identitymodel.js
+++ medicom/server/identitymodel.js
@@ -32,16 +32,18 @@ IdentityModel.prototype.__minute2milli = function(min)
 
 IdentityModel.prototype.verify_identity = function(identity)
 {
-        if (identity == null) return false;
+        if (identity == null)
+                throw Error("No identity specified");
         var result = this.__identities.find({__session_id : identity.get_session_id()});
-        if (result.count() == 0) return false;
+        if (result.count() == 0)
+                throw Error("Identity doesn't exist in our record");
 
         var db_iden = Identity_create_from_POD(result.fetch()[0]);
         var db_account = db_iden.get_account_record();
         var param_account = identity.get_account_record();
         if (db_account.get_account_id() !== param_account.get_account_id() ||
-                                            !db_account.verify_internal_pass(param_account.get_internal_pass()) ||
-                                            !db_account.is_activated()) {
+            !db_account.verify_internal_pass(param_account.get_internal_pass()) ||
+            !db_account.is_active()) {
                 return false;
         }
 
@@ -59,25 +61,32 @@ IdentityModel.prototype.verify_identity = function(identity)
 
 IdentityModel.prototype.create_identity = function(record)
 {
+        if (record == null)
+                throw new Error("Logic error: creating an identity with null record");
         return new Identity(this.__mongo.get_string_uuid(), record);
+}
+
+IdentityModel.prototype.__elevate = function(identity, record)
+{
+        if (record == null)
+                throw Error("Logic error: elevating to a null record");
+        identity.elevate(record);
+        this.__identities.insert(identity);
+        return identity;
 }
 
 IdentityModel.prototype.elevate_by_user_password = function(identity, record, password)
 {
         if (!record.verify_password(password))
                 throw Error("Invalid password");
-        identity.elevate(record);
-        this.__identities.insert(identity);
-        return identity;
+        return this.__elevate(identity, record);
 }
 
 IdentityModel.prototype.elevate_by_identity_auth_code = function(identity, auth_code, record)
 {
         if (!identity.get_account_record().verify_auth_code(auth_code))
-                throw Error("Invalid auth code");
-        identity.elevate(record);
-        this.__identities.insert(identity);
-        return identity;
+                throw Error("Invalid auth code: " + auth_code);
+        return this.__elevate(identity, record);
 }
 
 IdentityModel.prototype.descend = function(identity)
