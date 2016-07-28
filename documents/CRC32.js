8dd61d85d0bf583772c0651ccfcec6f4df586545
Zhaonian Luan <zhaonial@uci.edu>
2016-07-28 13:48:58 -0700

    Finish CRC32.js

---
+++ documents/CRC32.js
@@ -0,0 +1,12 @@
+function CRC32(data) {
+	int divisor = 1011;
+	int new_data;
+	for (i < int.toString().length) {
+		for (j < divisor) {
+			if (data[i + j] == divisor[j]) {
+				new_data[i + j] = 0;
+				} else {new_data[i + j] = 1}
+			data[i + j] = new_data[i + j];
+		} 
+	}	
+}
