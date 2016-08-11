4aeb0f03adc59fca958a44775e8295353cc07581
Chifeng Wen <daviesx66@gmail.com>
2016-08-10 20:28:06 -0700

    Organized object AccountInfo. Also added is_equal() to it.

--- medicom/api/accountinfo.js
+++ medicom/api/accountinfo.js
@@ -11,18 +11,46 @@
  * You should have received a copy of the GNU General Public License along with this program; if not,
  * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
-export function AccountInfo(record, account_id, name, email) {
+
+export function AccountInfo(record, account_id, name, email)
+{
         this.__record = record;
         this.__account_id = account_id;
         this.__name = name;
         this.__email = email;
-        this.get_record = function() { return this.__record; }
-        this.get_account_id = function() { return this.__account_id; }
-        this.get_name = function() { return this.__name; }
-        this.get_email = function() { return this.__email; }
+
 }
 
-export function AccountInfo_Create_From_POD(pod) {
+AccountInfo.prototype.is_equal = function(info)
+{
+        return this.__record.is_equal(info.__record) &&
+               this.__account_id == info.__account_id &&
+               this.__name == info.__name &&
+               this.__email == info.__email;
+}
+
+AccountInfo.prototype.get_record = function()
+{
+        return this.__record;
+}
+
+AccountInfo.prototype.get_account_id = function()
+{
+        return this.__account_id;
+}
+
+AccountInfo.prototype.get_name = function()
+{
+        return this.__name;
+}
+
+AccountInfo.prototype.get_email = function()
+{
+        return this.__email;
+}
+
+export function AccountInfo_Create_From_POD(pod)
+{
         var obj = new AccountInfo(null, 0, null, null);
         obj.__record = pod.__record;
         obj.__account_id = pod.__account_id;
