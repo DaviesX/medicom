6e23de09c8efa17ebe6dd40d5a2ed019f623fc42
Chifeng Wen <daviesx66@gmail.com>
2016-08-10 21:21:55 -0700

    Organized object UserBrowser.

--- medicom/client/js/userbrowser.js
+++ medicom/client/js/userbrowser.js
@@ -17,7 +17,8 @@ import {SequentialEffect} from "./effects.js";
 import {G_Session} from "./session.js";
 
 
-export function UserBrowser() {
+export function UserBrowser()
+{
         this.__identity = null;
         this.__on_update = null;
         this.__on_select = null;
@@ -28,142 +29,158 @@ export function UserBrowser() {
 
         this.__users = [];
         this.__selected_user_id = -1;
+}
 
-        this.__register_on_click = function() {
-                var elms = document.getElementsByName("user-list");
-                for (var i = 0; i < elms.length; i ++) {
-                        var clazz = this;
-                        elms[i].addEventListener("click", function(event) {
-                                var account_id = parseInt((event.target || event.srcElement).id, 10);
-                                clazz.__selected_user_id = account_id;
-                                clazz.__on_select(clazz);
-                        }, false);
-                }
-        }
 
-        this.__animate_effect = function() {
-                var elms = document.getElementsByName("user-list");
-                var seq_effect = new SequentialEffect("fade");
-                for (var i = 0; i < elms.length; i ++) {
-                        seq_effect.add_elm($(elms[i]));
-                }
-                seq_effect.finalize();
-                seq_effect.animate();
-        }
-
-        this.__make_user_ui = function(user_id, user_name) {
-                return '<div><button class="simp_classic-user-button" name="user-list" id="' + user_id + '">' +
-                        user_id + '. ' + user_name + '</button></div>';
-        }
-
-        this.register_on_update = function(call, params) {
-                this.__on_update = {call: call, params: params};
-        }
-
-        this.register_on_add_user = function(call, params) {
-                this.__on_add_user = {call: call, params: params};
-        }
-
-        this.set_browser_on_select = function(callback) {
-                this.__on_select = callback;
-        }
-
-        this.set_identity = function(identity) {
-                this.__identity = identity;
-        }
-
-        this.set_user_list_holder = function(holder) {
-                this.__user_list_holder = holder;
-        }
-
-        this.set_add_user_button = function(holder) {
-                this.__add_user_button = holder;
-        }
-
-        this.set_add_user_dialog = function(holder) {
-                this.__add_user_dialog = holder;
-                var submit_btn = this.__add_user_dialog.find("button[name='submit']");
-                var input_field = this.__add_user_dialog.find("input[name='input-field']");
-                if (submit_btn == null) throw "The dialog doesn't contain a submit button";
-                if (input_field == null) throw "The dialog doesn't contain an input field";
-
+UserBrowser.prototype.__register_on_click = function()
+{
+        var elms = document.getElementsByName("user-list");
+        for (var i = 0; i < elms.length; i ++) {
                 var clazz = this;
-                submit_btn.click(function() {
-                        clazz.__add_user_dialog.dialog("close");
-                        var account_id = parseInt(input_field.val(), 10);
-                        var email = input_field.val();
-
-                        clazz.__on_add_user.params.identity = clazz.__identity;
-                        clazz.__on_add_user.params.id = account_id;
-                        clazz.__on_add_user.params.email = email;
-
-                        Meteor.call(clazz.__on_add_user.call, clazz.__on_add_user.params, function(error, result) {
-                                if (result.error != "") {
-                                        alert("Failed to add user: " + result.error);
-                                } else {
-                                        clazz.update_user_list();
-                                        alert("User has been added");
-                                }
-                        });
-                });
+                elms[i].addEventListener("click", function(event) {
+                        var account_id = parseInt((event.target || event.srcElement).id, 10);
+                        clazz.__selected_user_id = account_id;
+                        clazz.__on_select(clazz);
+                }, false);
         }
+}
 
-        this.enable_add_user = function(to_enable, prompt) {
-                if (!to_enable) {
-                        this.__add_user_button.css("display", "none");
-                } else {
-                        var clazz = this;
-                        this.__add_user_button.css("display", "inline");
-                        this.__add_user_button.html(prompt);
-                        this.__add_user_button.click(function () {
-                                clazz.__add_user_dialog.dialog({
-                                        autoOpen: true,
-                                        modal: true,
-                                        open: function() {
-                                                $(".ui-widget-overlay").addClass("simp_dialog_overlay");
-                                                $(".ui-dialog-titlebar").css("display", "none");
-                                        },
-                                        show: {
-                                                effect: "fade",
-                                                duration: 1000,
-                                        },
-                                        hide: {
-                                                effect: "fade",
-                                                duration: 1000,
-                                        },
-                                        close: function() {
-                                        },
-                                });
-                        });
-                }
+UserBrowser.prototype.__animate_effect = function()
+{
+        var elms = document.getElementsByName("user-list");
+        var seq_effect = new SequentialEffect("fade");
+        for (var i = 0; i < elms.length; i ++) {
+                seq_effect.add_elm($(elms[i]));
         }
+        seq_effect.finalize();
+        seq_effect.animate();
+}
 
-        this.update_user_list = function() {
-                var clazz = this;
-                this.__on_update.params.identity = this.__identity;
-                this.__on_update.params.id = this.__selected_user_id;
+UserBrowser.prototype.__make_user_ui = function(user_id, user_name)
+{
+        return '<div><button class="simp_classic-user-button" name="user-list" id="' + user_id + '">' +
+               user_id + '. ' + user_name + '</button></div>';
+}
 
-                Meteor.call(this.__on_update.call, this.__on_update.params, function(error, result) {
+UserBrowser.prototype.register_on_update = function(call, params)
+{
+        this.__on_update = {call: call, params: params};
+}
+
+UserBrowser.prototype.register_on_add_user = function(call, params)
+{
+        this.__on_add_user = {call: call, params: params};
+}
+
+UserBrowser.prototype.set_browser_on_select = function(callback)
+{
+        this.__on_select = callback;
+}
+
+UserBrowser.prototype.set_identity = function(identity)
+{
+        this.__identity = identity;
+}
+
+UserBrowser.prototype.set_user_list_holder = function(holder)
+{
+        this.__user_list_holder = holder;
+}
+
+UserBrowser.prototype.set_add_user_button = function(holder)
+{
+        this.__add_user_button = holder;
+}
+
+UserBrowser.prototype.set_add_user_dialog = function(holder)
+{
+        this.__add_user_dialog = holder;
+        var submit_btn = this.__add_user_dialog.find("button[name='submit']");
+        var input_field = this.__add_user_dialog.find("input[name='input-field']");
+        if (submit_btn == null) throw "The dialog doesn't contain a submit button";
+        if (input_field == null) throw "The dialog doesn't contain an input field";
+
+        var clazz = this;
+        submit_btn.click(function() {
+                clazz.__add_user_dialog.dialog("close");
+                var account_id = parseInt(input_field.val(), 10);
+                var email = input_field.val();
+
+                clazz.__on_add_user.params.identity = clazz.__identity;
+                clazz.__on_add_user.params.id = account_id;
+                clazz.__on_add_user.params.email = email;
+
+                Meteor.call(clazz.__on_add_user.call, clazz.__on_add_user.params, function(error, result) {
                         if (result.error != "") {
-                                console.log(result.error);
+                                alert("Failed to add user: " + result.error);
                         } else {
-                                clazz.__user_list_holder.empty();
-                                for (var i = 0; i < result.account_infos.length; i ++) {
-                                        var account_info = AccountInfo_Create_From_POD(result.account_infos[i]);
-                                        var user_id = account_info.get_account_id();
-                                        var ui = clazz.__make_user_ui(user_id, account_info.get_name());
-                                        clazz.__user_list_holder.append(ui);
-                                        clazz.__users[user_id] = account_info;
-                                }
-                                clazz.__register_on_click();
-                                clazz.__animate_effect();
+                                clazz.update_user_list();
+                                alert("User has been added");
                         }
                 });
-        }
+        });
+}
 
-        this.get_selected_user = function() {
-                return this.__users[this.__selected_user_id];
+UserBrowser.prototype.enable_add_user = function(to_enable, prompt)
+{
+        if (!to_enable) {
+                this.__add_user_button.css("display", "none");
+        } else {
+                var clazz = this;
+                this.__add_user_button.css("display", "inline");
+                this.__add_user_button.html(prompt);
+                this.__add_user_button.click(function () {
+                        clazz.__add_user_dialog.dialog({
+                                autoOpen: true,
+                                modal: true,
+                                open: function()
+                                {
+                                        $(".ui-widget-overlay").addClass("simp_dialog_overlay");
+                                        $(".ui-dialog-titlebar").css("display", "none");
+                                },
+                                show: {
+                                        effect: "fade",
+                                        duration: 1000,
+                                },
+                                hide: {
+                                        effect: "fade",
+                                        duration: 1000,
+                                },
+                                close: function()
+                                {
+                                },
+                        });
+                });
         }
+}
+
+UserBrowser.prototype.update_user_list = function()
+{
+        var clazz = this;
+        this.__on_update.params.identity = this.__identity;
+        this.__on_update.params.id = this.__selected_user_id;
+
+        Meteor.call(this.__on_update.call, this.__on_update.params, function(error, result) {
+                if (result.error != "") {
+                        console.log(result.error);
+                } else {
+                        clazz.__user_list_holder.empty();
+                        for (var i = 0; i < result.account_infos.length; i ++) {
+                                var account_info = AccountInfo_Create_From_POD(result.account_infos[i]);
+                                var user_id = account_info.get_account_id();
+                                var ui = clazz.__make_user_ui(user_id, account_info.get_name());
+                                clazz.__user_list_holder.append(ui);
+                                clazz.__users[user_id] = account_info;
+                        }
+                        clazz.__register_on_click();
+                        clazz.__animate_effect();
+                }
+        });
+}
+
+UserBrowser.prototype.get_selected_user = function()
+{
+        return this.__users[this.__selected_user_id];
 }
 
 export var G_UserBrowser = new UserBrowser();
