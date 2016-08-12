78b4f0f5423b7413861d6ae7bd8dc0bb03d1be8d
Chifeng Wen <daviesx66@gmail.com>
2016-08-12 12:38:06 -0700

    Organized code Blood Pressure Display.

--- medicom/client/js/bpdisplay.js
+++ medicom/client/js/bpdisplay.js
@@ -13,7 +13,8 @@
  */
 import {ValueTable, ValueTable_create_from_POD} from "../../api/valuetable.js";
 
-export function BloodPressureDisplay() {
+export function BloodPressureDisplay() 
+{
         this.__identity = null;
         this.__browsing_user = null;
         this.__session = null;
@@ -26,227 +27,243 @@ export function BloodPressureDisplay() {
         this.__file = null;
 
         this.__local_bptable = null;
+}
 
-        this.set_access_info = function(identity, browsing_user, session) {
-                this.__identity = identity
-                this.__browsing_user = browsing_user;
-                this.__session = session;
-        }
+BloodPressureDisplay.prototype.set_access_info = function(identity, browsing_user, session)
+{
+        this.__identity = identity
+        this.__browsing_user = browsing_user;
+        this.__session = session;
+}
 
-        this.__get_file_from_file_select = function(holder) {
-                var files = holder.prop("files");
-                if (files == null || files.length == 0)
-                        return null;
-                else
-                        return files[0];
-        }
+BloodPressureDisplay.prototype.__get_file_from_file_select = function(holder)
+{
+        var files = holder.prop("files");
+        if (files == null || files.length == 0)
+                return null;
+        else
+                return files[0];
+}
 
-        // Holders
-        this.set_file_select_holder = function(holder, disconnector, filepath_holder) {
+// Holders
+BloodPressureDisplay.prototype.set_file_select_holder = function(holder, disconnector, filepath_holder)
+{
+        filepath_holder.html("No file is connected");
+
+        var clazz = this;
+
+        holder.on("change", function (event) {
+                var file = clazz.__get_file_from_file_select(holder);
+                if (file == null)
+                        return;
+                filepath_holder.html(file.name);
+                clazz.__file = file;
+                clazz.update();
+        });
+
+        disconnector.on("click", function(event) {
                 filepath_holder.html("No file is connected");
+                holder.replaceWith(holder = holder.clone(true));
+                clazz.__file = null;
+                clazz.update();
+        });
+}
 
-                var clazz = this;
+BloodPressureDisplay.prototype.set_date_holder = function(start, end)
+{
+        var clazz = this;
 
-                holder.on("change", function (event) {
-                        var file = clazz.__get_file_from_file_select(holder);
-                        if (file == null)
-                                return;
-                        filepath_holder.html(file.name);
-                        clazz.__file = file;
-                        clazz.update();
-                });
+        start.datepicker().on("change", function (e) {
+                clazz.__start_date = new Date(e.target.value);
+                clazz.update();
+        });
+        end.datepicker().on("change", function(e) {
+                clazz.__end_date = new Date(e.target.value);
+                clazz.update();
+        });
+}
 
-                disconnector.on("click", function(event) {
-                        filepath_holder.html("No file is connected");
-                        holder.replaceWith(holder = holder.clone(true));
-                        clazz.__file = null;
-                        clazz.update();
-                });
-        }
+BloodPressureDisplay.prototype.set_charting_area = function(holder)
+{
+        this.__charting_area = holder;
+}
 
-        this.set_date_holder = function(start, end) {
-                var clazz = this;
+BloodPressureDisplay.prototype.set_filter_holder = function(holder)
+{
+        var clazz = this;
 
-                start.datepicker().on("change", function (e) {
-                        clazz.__start_date = new Date(e.target.value);
-                        clazz.update();
-                });
-                end.datepicker().on("change", function(e) {
-                        clazz.__end_date = new Date(e.target.value);
-                        clazz.update();
-                });
-        }
+        holder.on("change", function(e) {
+                clazz.__filtering = e.target.value;
+                clazz.update();
+        });
+}
 
-        this.set_charting_area = function(holder) {
-                this.__charting_area = holder;
-        }
+BloodPressureDisplay.prototype.set_sample_count_holder = function(holder)
+{
+        var clazz = this;
 
-        this.set_filter_holder = function(holder) {
-                var clazz = this;
+        holder.on("change", function(e) {
+                clazz.__sample_count = e.target.value == "" ? null : parseInt(e.target.value);
+                clazz.update();
+        });
+}
 
-                holder.on("change", function(e) {
-                        clazz.__filtering = e.target.value;
-                        clazz.update();
-                });
-        }
+// Data.
+BloodPressureDisplay.prototype.get_processed_table = function(start_date, end_date, num_samples, filter)
+{
+        if (this.__local_bptable == null)
+                return null;
+        var bptable = this.__local_bptable;
 
-        this.set_sample_count_holder = function(holder) {
-                var clazz = this;
+        bptable = bptable.sort_data(false);
 
-                holder.on("change", function(e) {
-                        clazz.__sample_count = e.target.value == "" ? null : parseInt(e.target.value);
-                        clazz.update();
-                });
-        }
-
-        // Data.
-        this.get_processed_table = function(start_date, end_date, num_samples, filter) {
-                if (this.__local_bptable == null)
-                        return null;
-                var bptable = this.__local_bptable;
-
-                bptable = bptable.sort_data(false);
-
-                // merge the data from the same day.
-                bptable = bptable.merge_adjacent_data(
-                        {
-                                name: filter,
-                                scalar: function(v) { return (v.systolic + v.diastolic)/2; },
-                                add: function(r, s) { return {systolic: r.systolic + s.systolic,
-                                                              diastolic: r.diastolic + s.diastolic}; },
-                                scale: function(k, v) { return {systolic: k*v.systolic,
-                                                                diastolic: k*v.diastolic}; },
-                        },
-                        function (a, b) {
-                                return a.getYear() == b.getYear() &&
-                                       a.getMonth() == b.getMonth() &&
-                                       a.getDate() == b.getDate();
-                        }
-                );
-                bptable = bptable.sample(start_date, end_date, num_samples);
-                return bptable;
-        }
-
-        this.generate_bp_renderable = function(bptable, target) {
-                var x = ["x"];
-                var y = ["systolic blood pressure"];
-                var z = ["diastolic blood pressure"];
-                var pairs = bptable.get_pairs();
-                for (var i = 0; i < pairs.length; i ++) {
-                        x[i + 1] = pairs[i].date;
-                        y[i + 1] = pairs[i].value.systolic.toFixed(1);
-                        z[i + 1] = pairs[i].value.diastolic.toFixed(1);
+        // merge the data from the same day.
+        bptable = bptable.merge_adjacent_data(
+                {
+                        name: filter,
+                        scalar: function(v) { return (v.systolic + v.diastolic)/2; },
+                        add: function(r, s) { return {systolic: r.systolic + s.systolic,
+                                                      diastolic: r.diastolic + s.diastolic}; },
+                        scale: function(k, v) { return {systolic: k*v.systolic,
+                                                        diastolic: k*v.diastolic}; },
+                },
+                function (a, b) {
+                        return a.getYear() == b.getYear() &&
+                               a.getMonth() == b.getMonth() &&
+                               a.getDate() == b.getDate();
                 }
+        );
+        bptable = bptable.sample(start_date, end_date, num_samples);
+        return bptable;
+}
 
-                return {
-                        bindto: target,
-                        data: {
-                                x: "x",
-                                columns: [x, y, z]
-                        },
-                        axis: {
-                                x: {
-                                        type: "timeseries",
-                                        tick: {
-                                                format: "%Y-%m-%d"
-                                        }
+BloodPressureDisplay.prototype.generate_bp_renderable = function(bptable, target)
+{
+        var x = ["x"];
+        var y = ["systolic blood pressure"];
+        var z = ["diastolic blood pressure"];
+        var pairs = bptable.get_pairs();
+        for (var i = 0; i < pairs.length; i ++) {
+                x[i + 1] = pairs[i].date;
+                y[i + 1] = pairs[i].value.systolic.toFixed(1);
+                z[i + 1] = pairs[i].value.diastolic.toFixed(1);
+        }
+
+        return {
+                bindto: target,
+                data: {
+                        x: "x",
+                        columns: [x, y, z]
+                },
+                axis: {
+                        x: {
+                                type: "timeseries",
+                                tick: {
+                                        format: "%Y-%m-%d"
                                 }
                         }
-                };
+                }
+        };
+}
+
+BloodPressureDisplay.prototype.set_local_data_from_file_stream = function(file, f_On_Complete)
+{
+        var fr = new FileReader();
+        var clazz = this;
+        this.clear_local_data();
+
+        fr.onload = function (e) {
+                var parts = file.name.split(".");
+                var suffix = parts[parts.length - 1];
+                var stream = e.target.result;
+
+                var bptable = new ValueTable();
+                bptable.construct_from_stream(suffix, stream);
+                clazz.__local_bptable = bptable;
+
+                if(f_On_Complete != null)
+                        f_On_Complete(clazz);
         }
+        fr.readAsText(file);
+}
 
-        this.set_local_data_from_file_stream = function(file, f_On_Complete) {
-                var fr = new FileReader();
-                var clazz = this;
-                this.clear_local_data();
+BloodPressureDisplay.prototype.set_local_data_from_remote_server = function(start_date, end_date, num_samples, f_On_Complete)
+{
+        this.clear_local_data();
 
-                fr.onload = function (e) {
-                        var parts = file.name.split(".");
-                        var suffix = parts[parts.length - 1];
-                        var stream = e.target.result;
-
-                        var bptable = new ValueTable();
-                        bptable.construct_from_stream(suffix, stream);
-                        clazz.__local_bptable = bptable;
-
-                        if(f_On_Complete != null)
+        var params = {
+                identity: this.__identity,
+                session_id: this.__session.get_session_id(),
+                start_date: start_date,
+                end_date: end_date,
+                num_samples: null,
+        };
+        var clazz = this;
+        Meteor.call("get_measure_bp_table", params, function(error, result) {
+                if (result.error != "") {
+                        console.log("failed to obtain bptable from patient: " + JSON.stringify(params));
+                } else {
+                        clazz.__local_bptable = ValueTable_create_from_POD(result.bptable);
+                        if (f_On_Complete != null)
                                 f_On_Complete(clazz);
                 }
-                fr.readAsText(file);
+        });
+}
+
+BloodPressureDisplay.prototype.clear_local_data = function()
+{
+        this.__local_bptable = null;
+}
+
+BloodPressureDisplay.prototype.render_local_data = function(start_date, end_date, filter, num_samples, target)
+{
+        if (this.__local_bptable) {
+                var bptable = this.get_processed_table(start_date, end_date, num_samples, filter);
+                c3.generate(this.generate_bp_renderable(bptable, target));
         }
+}
 
-        this.set_local_data_from_remote_server = function(start_date, end_date, num_samples, f_On_Complete) {
-                this.clear_local_data();
-
-                var params = {
-                        identity: this.__identity,
-                        session_id: this.__session.get_session_id(),
-                        start_date: start_date,
-                        end_date: end_date,
-                        num_samples: null,
-                };
-                var clazz = this;
-                Meteor.call("get_measure_bp_table", params, function(error, result) {
-                        if (result.error != "") {
-                                console.log("failed to obtain bptable from patient: " + JSON.stringify(params));
-                        } else {
-                                clazz.__local_bptable = ValueTable_create_from_POD(result.bptable);
-                                if (f_On_Complete != null)
-                                        f_On_Complete(clazz);
-                        }
+BloodPressureDisplay.prototype.update = function()
+{
+        var clazz = this;
+        if (this.__file != null) {
+                this.set_local_data_from_file_stream(this.__file, function(obj) {
+                        clazz.render_local_data(clazz.__start_date, clazz.__end_date,
+                                                clazz.__filtering, clazz.__sample_count,
+                                                clazz.__charting_area);
+                });
+        } else {
+                this.set_local_data_from_remote_server(this.__start_date, this.__end_date,
+                                                       this.__sample_count, function(obj) {
+                        clazz.render_local_data(clazz.__start_date, clazz.__end_date,
+                                                clazz.__filtering, clazz.__sample_count,
+                                                clazz.__charting_area);
                 });
         }
+}
 
-        this.clear_local_data = function() {
-                this.__local_bptable = null;
-        }
+BloodPressureDisplay.prototype.upload_to_remote_server = function()
+{
+        if (this.__local_bptable == null) return false;
 
-        this.render_local_data = function(start_date, end_date, filter, num_samples, target) {
-                if (this.__local_bptable) {
-                        var bptable = this.get_processed_table(start_date, end_date, num_samples, filter);
-                        c3.generate(this.generate_bp_renderable(bptable, target));
-                }
-        }
-
-        this.update = function() {
-                var clazz = this;
-                if (this.__file != null) {
-                        this.set_local_data_from_file_stream(this.__file, function(obj) {
-                                clazz.render_local_data(clazz.__start_date, clazz.__end_date,
-                                                        clazz.__filtering, clazz.__sample_count,
-                                                        clazz.__charting_area);
-                        });
+        var params = {
+                identity: this.__identity,
+                session_id: this.__session.get_session_id(),
+                bptable: this.__local_bptable,
+        };
+        Meteor.call("update_measure_bp_from_table", params, function(error, result) {
+                if (result.error != "") {
+                        alert(result.error);
+                        console.log("failed to upload bp table: " + JSON.stringify(params));
                 } else {
-                        this.set_local_data_from_remote_server(this.__start_date, this.__end_date,
-                                                               this.__sample_count, function(obj) {
-                                clazz.render_local_data(clazz.__start_date, clazz.__end_date,
-                                                        clazz.__filtering, clazz.__sample_count,
-                                                        clazz.__charting_area);
-                        });
+                        console.log("remote blood pressure data has been updated");
                 }
-        }
+        });
+        return true;
+}
 
-        this.upload_to_remote_server = function() {
-                if (this.__local_bptable == null) return false;
-
-                var params = {
-                        identity: this.__identity,
-                        session_id: this.__session.get_session_id(),
-                        bptable: this.__local_bptable,
-                };
-                Meteor.call("update_measure_bp_from_table", params, function(error, result) {
-                        if (result.error != "") {
-                                alert(result.error);
-                                console.log("failed to upload bp table: " + JSON.stringify(params));
-                        } else {
-                                console.log("remote blood pressure data has been updated");
-                        }
-                });
-                return true;
-        }
-
-        this.save_local_to_file_stream = function(file) {
-        }
+BloodPressureDisplay.prototype.save_local_to_file_stream = function(file)
+{
 }
 
 export var G_BPDisplay = new BloodPressureDisplay();
