7c519496b9259861a4e135f459eeb7d1cade5090
Chifeng Wen <daviesx66@gmail.com>
2016-09-05 20:05:42 -0700

    Converted ErrorMessageQueue to ts class.

---
+++ medicom/api/error.ts
@@ -0,0 +1,52 @@
+/*
+ * This file is part of MediCom
+ *
+ * Copyright © 2016, Chifeng Wen.
+ * MediCom is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License, version 2, as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+export class ErrorMessages
+{
+        public m_queue:        Array<string>;
+
+        constructor() {}
+        
+        public log(message: string): void
+        {
+                this.m_queue.push(message);
+        }
+        
+        public is_empty(): boolean 
+        {
+                return this.fetch_all().length == 0;
+        }
+        
+        public fetch_all(): Array<string> 
+        {
+                var answer = [];
+                for (var i = 0, j = 0; i < this.m_queue.length; i ++) {
+                        if (this.m_queue[i] != null || this.m_queue[i] == "")
+                                answer[j ++] = this.m_queue[i];
+                }
+                return answer;
+        }
+        
+        public clear(): void
+        {
+                this.m_queue = [];
+        }
+}
+
+export function error_messages_copy(pod): ErrorMessages
+{
+        var obj = new ErrorMessages();
+        obj.m_queue = pod.m_queue;
+        return obj;
+}
