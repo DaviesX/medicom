c09891234e777b49732f65c77961cf8101b78dd3
Chifeng Wen <daviesx66@gmail.com>
2016-09-15 05:46:49 -0700

    Converted Privilege to ts class.

--- medicom/api/privilege.ts
+++ medicom/api/privilege.ts
@@ -12,12 +12,7 @@
  * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-import {c_UserGroup_Root,
-        c_UserGroup_Admin,
-        c_UserGroup_Provider,
-        c_UserGroup_Patient,
-        c_UserGroup_Assistant,
-        c_UserGroup_Temporary} from "./usergroup.js";
+import {UserGroupConst} from "./usergroup.ts";
 
 // User group actions
 export const c_Root_Actions = [
@@ -57,12 +52,12 @@ export const c_Admin_Actions = [
 ];
 
 export const c_Assistant_Actions = [
-        {action: "register account",            scope: [-2, c_UserGroup_Patient], grant_option: false},
-        {action: "activate account",            scope: [-2, c_UserGroup_Patient], grant_option: false},
+        {action: "register account",            scope: [-2, UserGroupConst.Patient], grant_option: false},
+        {action: "activate account",            scope: [-2, UserGroupConst.Patient], grant_option: false},
         {action: "search account",              scope: [-2], grant_option: false},
         {action: "view profile",                scope: [-2], grant_option: false},
         {action: "view session",                scope: [-2], grant_option: false},
-        {action: "create association",          scope: [-2, c_UserGroup_Patient], grant_option: false},
+        {action: "create association",          scope: [-2, UserGroupConst.Patient], grant_option: false},
         {action: "remove association",          scope: [-1], grant_option: false},
         {action: "search association",          scope: [-1], grant_option: false},
         {action: "add session",                 scope: [-2], grant_option: false},
@@ -73,11 +68,11 @@ export const c_Assistant_Actions = [
 ];
 
 export const c_Provider_Actions = [
-        {action: "register account",            scope: [-2, c_UserGroup_Patient], grant_option: false},
-        {action: "activate account",            scope: [-2, c_UserGroup_Patient], grant_option: false},
-        {action: "search account",              scope: [-2, c_UserGroup_Patient], grant_option: false},
+        {action: "register account",            scope: [-2, UserGroupConst.Patient], grant_option: false},
+        {action: "activate account",            scope: [-2, UserGroupConst.Patient], grant_option: false},
+        {action: "search account",              scope: [-2, UserGroupConst.Patient], grant_option: false},
         {action: "view profile",                scope: [-2], grant_option: false},
-        {action: "create association",          scope: [-2, c_UserGroup_Patient], grant_option: false},
+        {action: "create association",          scope: [-2, UserGroupConst.Patient], grant_option: false},
         {action: "remove association",          scope: [-1], grant_option: false},
         {action: "search association",          scope: [-1], grant_option: false},
         {action: "create session",              scope: [-1], grant_option: false},
@@ -109,42 +104,154 @@ export const c_Temporary_Actions = [
         {action: "register account",            scope: [-2], grant_option: false},
 ];
 
-
-// Privilege
-export function Privilege(actions)
+/*
+ * <PrivilegeAction>
+ */
+export class PrivilegeAction
 {
-        this.__actions = actions;
-}
+        public action:                string;
+        public scope_set:             Array<number>;
+        public with_grant_option:     boolean;
 
-Privilege.prototype.has_action = function(action, scope)
-{
-        return this.__find(action, scope) != null;
-}
-
-Privilege.prototype.all_actions = function()
-{
-        return this.__actions;
-}
-
-Privilege.prototype.__find = function(action, scope)
-{
-        for (var i = 0; i < this.__actions.length; i ++) {
-                if (this.__actions[i].action == action) {
-                        if (scope != null) {
-                                for (var j = 0; j < this.__actions[i].scope.length; j ++) {
-                                        if (this.__actions[i].scope[j] == scope)
-                                                return this.__actions[i];
-                                }
-                        } else
-                                return this.__actions[i];
-                }
+        // Parse special scopes.
+        private replace_special_scope(scope_set: Array<number>, node_ref: number): void
+        {
+                for (var i = 0; i < scope_set.length; i ++)
+                        if (scope_set[i] == -2)
+                                scope_set[i] = node_ref;
         }
-        return null;
+
+        constructor(node_ref: number, action: string, scope_set: Array<number>, with_grant_option: boolean)
+        {
+                this.action = action;
+                this.scope_set = scope_set;
+                this.with_grant_option = with_grant_option;
+
+                if (scope_set != null)
+                        this.replace_special_scope(this.scope_set, node_ref);
+        }
+
+        public get_action(): string
+        {
+                return this.action;
+        }
+        
+        public get_scope_set(): Array<number>
+        {
+                return this.scope_set;
+        }
+        
+        public set_grant_option(with_grant_option: boolean): void
+        {
+                this.with_grant_option = with_grant_option;
+        }
+        
+        public has_grant_option(): boolean
+        {
+                return this.with_grant_option;
+        }
+        
+        public add_scope(scope: number, node_ref: number): boolean 
+        {
+                if (this.is_in_scope(scope))
+                        return false;
+                this.scope_set.push(scope);
+                this.replace_special_scope(this.scope_set, node_ref);
+                return true;
+        }
+        
+        public remove_scope(scope: number): void
+        {
+                var target = this.scope_set.length;
+                for (var i = 0; i < this.scope_set.length; i ++) {
+                        if (this.scope_set[i] == scope) {
+                                target = i;
+                                break;
+                        }
+                }
+                var last;
+                for (last = target; last < this.scope_set.length - 1; last ++)
+                        this.scope_set[last] = this.scope_set[last + 1];
+                this.scope_set = this.scope_set.slice(last);
+        }
+        
+        public is_in_scope(scope: number): boolean
+        {
+                for (var i = 0; i < this.scope_set.length; i ++) {
+                        if (this.scope_set[i] == -1 || this.scope_set[i] == scope)
+                                return true;
+                }
+                return false;
+        }
+        
+        public is_inclusive_scope_set(scope_set: Array<number>): boolean
+        {
+                for (var i = 0; i < scope_set.length; i ++) {
+                        if (!this.is_in_scope(scope_set[i]))
+                                return false;
+                }
+                return true;
+        }
+        
+        public is_action_compatible(action: string): boolean
+        {
+                return this.action == action;
+        }
+};
+
+export function privilege_action_copy(pod): PrivilegeAction
+{
+        var obj = new PrivilegeAction(null, null, null, null);
+        obj.action = pod.action;
+        obj.scope_set = pod.scope_set;
+        obj.with_grant_option = pod.with_grant_option;
+        return obj;
 }
 
-export function Privilege_create_from_POD(pod)
+/*
+ * <Privilege> Sets of actions.
+ */
+export class Privilege
 {
-        var obj = new Privilege();
-        obj.__actions = pod.__actions;
+        public actions: Array<PrivilegeAction>;
+
+        constructor(actions)
+        {
+                this.actions = actions;
+        }
+
+        public has_action(action: string, scope: number): boolean
+        {
+                return this.find(action, scope) != null;
+        }
+        
+        public all_actions(): Array<PrivilegeAction>
+        {
+                return this.actions;
+        }
+        
+        public find(action: string, scope: number): PrivilegeAction
+        {
+                for (var i = 0; i < this.actions.length; i ++) {
+                        if (this.actions[i].action == action) {
+                                if (scope != null) {
+                                        for (var j = 0; j < this.actions[i].scope_set.length; j ++) {
+                                                if (this.actions[i].scope_set[j] == scope)
+                                                        return this.actions[i];
+                                        }
+                                } else
+                                        return this.actions[i];
+                        }
+                }
+                return null;
+        }
+};
+
+export function privilege_copy(pod): Privilege
+{
+        var obj = new Privilege(null);
+        obj.actions = pod.actions;
+        for (var i = 0; i < obj.actions.length; i ++)
+                obj.actions[i] = privilege_action_copy(obj.actions[i]);
         return obj;
 }
