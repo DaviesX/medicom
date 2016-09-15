4538ef3f5f0e9c2e218406c22bbd19c1fb20181d
Chifeng Wen <daviesx66@gmail.com>
2016-09-15 06:10:20 -0700

    Removed unused files.

--- medicom/api/sequscore.ts
+++ medicom/api/sequscore.ts
@@ -11,4 +11,11 @@
  * You should have received a copy of the GNU General Public License along with this program; if not,
  * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
-import {Meteor} from 'meteor/meteor';
+
+const INDEL_COST = -1;
+const MATCH_GAIN = 1;
+
+export function sequence_score(keyword: string, str: string): number
+{
+        return 0;
+}
